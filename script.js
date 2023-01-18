2ae4a6462ce3662818ae236fcbc1fea538c26d21
ignotoBR <91917734+ignotoBR@users.noreply.github.com>
2023-01-18 13:20:36 -0300

    Add files via upload
    Atualização 18/01

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ function prev(){
 
 setInterval(next, 10000);
 /*FIM PRIMEIRA PÁGINA*/
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "icones/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "icones/close_white_36dp.svg";
+    }
+}
